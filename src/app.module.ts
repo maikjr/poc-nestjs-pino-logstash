168c012a70886d4c14c43110aa6940f7dbb05a43
Maik Junior <maik.designer101@gmail.com>
2022-12-14 11:43:33 -0300

    add envairoments variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerModule } from 'nestjs-pino';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env'],
+    }),
     LoggerModule.forRoot({
       pinoHttp: {
+        name: 'nestjs-poc',
         transport: {
           target: 'pino-socket',
           options: {
             address: process.env.LOGASTASH_HOST,
             port: process.env.LOGASTASH_PORT,
-            mode: 'tcp',
+            mode: process.env.LOGASTASH_MODE,
+            //mode: 'tcp',
             //port: 5044,
-            //mode: 'udp',
           },
         },
       },
